9834c1f6a3f1413a354ae219adcfb01cb229d14b
SkyEvil006 <115894680+SkyEvil006@users.noreply.github.com>
2023-06-15 21:06:31 +0300

    Update settings.py

--- habranalog/habranalog/settings.py
+++ habranalog/habranalog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ksoehtop#@%7f9hldpfj*73-)^#^g%m+e7cw0tws3rrh7z60k7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
